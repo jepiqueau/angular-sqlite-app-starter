fb0fe05d0ca0f137a417808afe649ac3fda04aee
jepiqueau <jepi.queau@free.fr>
2021-08-25 19:02:30 +0200

    fix electron and web platforms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SQLiteService } from './services/sqlite.service';
 import { DetailService } from './services/detail.service';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-root',
@@ -22,20 +23,19 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(async () => {
-      await customElements.whenDefined('jeep-sqlite');
       this.detail.setExistingConnection(false);
       this.detail.setExportJson(false);
       this.sqlite.initializePlugin().then(async (ret) => {
         this.initPlugin = ret;
-        console.log(">>>> in App  this.initPlugin " + this.initPlugin)
+        const p: string = this.sqlite.platform;
+        console.log(`plaform ${p}`);
+        if( p === "web") {
+          this.isWeb = true;
+          await customElements.whenDefined('jeep-sqlite');
+        }
+          console.log(">>>> in App  this.initPlugin " + this.initPlugin)
       });
     });
   }
-  ionViewDidLoad() {
-    const p: string = this.sqlite.platform;
-    console.log(`plaform ${p}`);
-    if( p === "web") {
-      this.isWeb = true;
-    }
-  }
+
 }
