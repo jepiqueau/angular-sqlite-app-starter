b2ca7c9efb45baf2beb203141d347bbc0307c5a1
jepiqueau <jepi.queau@free.fr>
2021-09-15 17:50:18 +0200

    add connect to db from app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,8 +32,38 @@ export class AppComponent {
         if( p === "web") {
           this.isWeb = true;
           await customElements.whenDefined('jeep-sqlite');
+          const jeepSqliteEl = document.querySelector('jeep-sqlite');
+          if(jeepSqliteEl != null) {
+            console.log(`isStoreOpen ${await jeepSqliteEl.isStoreOpen()}`)
+            console.log(`$$ jeepSqliteEl is defined}`);
+          } else {
+            console.log('$$ jeepSqliteEl is null');
+          }
         }
-          console.log(">>>> in App  this.initPlugin " + this.initPlugin)
+        try {
+          console.log(`going to create a connection`)
+          const db = await this.sqlite.createConnection("db_issue9",false,"no-encryption", 1);
+          console.log(`db ${JSON.stringify(db)}`)
+          await db.open();
+          console.log(`after db.open`)
+          let query = `
+          CREATE TABLE IF NOT EXISTS test (
+            id INTEGER PRIMARY KEY NOT NULL,
+            name TEXT NOT NULL
+          );
+          `
+          console.log(`query ${query}`)
+
+          const res: any = await db.execute(query);
+          console.log(`res: ${JSON.stringify(res)}`)
+          await db.close();
+          console.log(`after db.close`)
+        } catch (err) {
+          console.log(`Error: ${err}`);
+          this.initPlugin = false;
+        }
+
+        console.log(">>>> in App  this.initPlugin " + this.initPlugin)
       });
     });
   }
