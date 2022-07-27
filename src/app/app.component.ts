489c19368abd2c61608d4fe14ac1a0e2cd75ed72
Mohammed Belhaj <m.belhaj@rs.almere>
2022-07-27 11:45:40 +0200

    fix initialize error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,80 +1,13 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SQLiteService } from './services/sqlite.service';
-import { DetailService } from './services/detail.service';
-import { Capacitor } from '@capacitor/core';
-import { App } from '@capacitor/app';
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  private initPlugin: boolean;
-  public isWeb: boolean = false;
   constructor(
-    private platform: Platform,
-    private sqlite: SQLiteService,
-    private detail: DetailService
   ) {
-    this.initializeApp();
-  }
-
-  initializeApp() {
-    this.platform.ready().then(async () => {
-      this.detail.setExistingConnection(false);
-      this.detail.setExportJson(false);
-      this.platform.backButton.subscribeWithPriority(
-        666666, () => {
-          App.exitApp();
-        });
-
-      this.sqlite.initializePlugin().then(async (ret) => {
-        this.initPlugin = ret;
-/*        const p: string = this.sqlite.platform;
-        console.log(`plaform ${p}`);
-        if (p === "web") {
-          this.isWeb = true;
-          await customElements.whenDefined('jeep-sqlite');
-          const jeepSqliteEl = document.querySelector('jeep-sqlite');
-          if (jeepSqliteEl != null) {
-            await this.sqlite.initWebStore();
-
-            console.log(`isStoreOpen ${await jeepSqliteEl.isStoreOpen()}`)
-            console.log(`$$ jeepSqliteEl is defined`);
-          } else {
-            console.log('$$ jeepSqliteEl is null');
-          }
-        }
-*/
-        try {
-          console.log(`going to create a connection`)
-          const db = await this.sqlite.createConnection("db_issue9", false, "no-encryption", 1);
-          console.log(`db ${JSON.stringify(db)}`)
-          await db.open();
-          console.log(`after db.open`)
-          let query = `
-          CREATE TABLE IF NOT EXISTS test (
-            id INTEGER PRIMARY KEY NOT NULL,
-            name TEXT NOT NULL
-          );
-          `
-          console.log(`query ${query}`)
-
-          const res: any = await db.execute(query);
-          console.log(`res: ${JSON.stringify(res)}`)
-          await this.sqlite.closeConnection("db_issue9");
-          console.log(`after closeConnection`)
-        } catch (err) {
-          console.log(`Error: ${err}`);
-          this.initPlugin = false;
-        }
-
-        console.log(">>>> in App  this.initPlugin " + this.initPlugin)
-      });
-    });
   }
 
 }
